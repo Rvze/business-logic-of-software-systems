647d5986c217ccbc2c590fb71d0db6f25f044431
nmakarov <311735@niuitmo.ru>
2023-03-27 21:41:18 +0300

    initial commit

---
+++ src/main/java/com/example/business_logic_of_software_systems/BusinessLogicOfSoftwareSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.business_logic_of_software_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLogicOfSoftwareSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessLogicOfSoftwareSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/business_logic_of_software_systems/BusinessLogicOfSoftwareSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.business_logic_of_software_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicOfSoftwareSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
